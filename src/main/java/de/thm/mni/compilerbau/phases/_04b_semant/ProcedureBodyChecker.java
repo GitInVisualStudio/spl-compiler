763b49ee7ddc6d6a0cf3dd73d4d5e3ae8212fa04
GitInVisualStudio <kaminund@gmail.com>
2022-11-16 12:59:51 +0100

    implementing TableBuilder & semantic analysis + corresponding test-files.

--- src/main/java/de/thm/mni/compilerbau/phases/_04b_semant/ProcedureBodyChecker.java
+++ src/main/java/de/thm/mni/compilerbau/phases/_04b_semant/ProcedureBodyChecker.java
@@ -2,9 +2,16 @@ package de.thm.mni.compilerbau.phases._04b_semant;
 
 import de.thm.mni.compilerbau.CommandLineOptions;
 import de.thm.mni.compilerbau.absyn.*;
-import de.thm.mni.compilerbau.table.SymbolTable;
+import de.thm.mni.compilerbau.absyn.visitor.DoNothingVisitor;
+import de.thm.mni.compilerbau.table.*;
+import de.thm.mni.compilerbau.types.ArrayType;
+import de.thm.mni.compilerbau.types.PrimitiveType;
 import de.thm.mni.compilerbau.types.Type;
 import de.thm.mni.compilerbau.utils.NotImplemented;
+import de.thm.mni.compilerbau.utils.SplError;
+
+import java.util.Optional;
+import java.util.SortedMap;
 
 /**
  * This class is used to check if the currently compiled SPL program is semantically valid.
@@ -12,18 +19,141 @@ import de.thm.mni.compilerbau.utils.NotImplemented;
  * Each node has to be checked for type issues or other semantic issues.
  * Calculated {@link Type}s can be stored in and read from the dataType field of the {@link Expression} and {@link Variable} classes.
  */
-public class ProcedureBodyChecker {
+public class ProcedureBodyChecker extends DoNothingVisitor {
 
     private final CommandLineOptions options;
+    private SymbolTable table;
 
     public ProcedureBodyChecker(CommandLineOptions options) {
         this.options = options;
     }
 
     public void checkProcedures(Program program, SymbolTable globalTable) {
-        //TODO (assignment 4b): Check all procedure bodies for semantic errors
+        this.table = globalTable;
+        // only visit the procedures
+        program.declarations.stream().filter(x -> x instanceof ProcedureDeclaration).forEach(x -> {
+            x.accept(this);
+        });
+    }
 
-        throw new NotImplemented();
+    @Override
+    public void visit(ProcedureDeclaration procedureDeclaration) {
+        ProcedureEntry entry = (ProcedureEntry) table.lookup(procedureDeclaration.name);
+        SymbolTable global = table;
+        table = entry.localTable;
+        procedureDeclaration.body.forEach(x -> x.accept(this));
+        table = global;
+    }
+
+    @Override
+    public void visit(WhileStatement whileStatement) {
+        whileStatement.condition.accept(this);
+        if (whileStatement.condition.dataType != PrimitiveType.boolType)
+            throw SplError.WhileConditionMustBeBoolean(whileStatement.position, whileStatement.condition.dataType);
+        whileStatement.body.accept(this);
+    }
+
+    @Override
+    public void visit(IfStatement ifStatement) {
+        ifStatement.condition.accept(this);
+        if (ifStatement.condition.dataType != PrimitiveType.boolType)
+            throw SplError.IfConditionMustBeBoolean(ifStatement.position, ifStatement.condition.dataType);
+        ifStatement.thenPart.accept(this);
+        ifStatement.elsePart.accept(this);
+    }
+
+    @Override
+    public void visit(CallStatement callStatement) {
+        Optional<Entry> result = table.find(callStatement.procedureName);
+        if (result.isEmpty())
+            throw SplError.UndefinedProcedure(callStatement.position, callStatement.procedureName);
+        if (!(result.get() instanceof ProcedureEntry))
+            throw SplError.CallOfNonProcedure(callStatement.position, callStatement.procedureName);
+        ProcedureEntry entry = (ProcedureEntry) table.lookup(callStatement.procedureName);
+        callStatement.arguments.forEach(x -> x.accept(this));
+
+        if (callStatement.arguments.size() > entry.parameterTypes.size())
+            throw SplError.TooManyArguments(callStatement.position, callStatement.procedureName);
+        if (callStatement.arguments.size() < entry.parameterTypes.size())
+            throw SplError.TooFewArguments(callStatement.position, callStatement.procedureName);
+
+        for (int i = 0; i < callStatement.arguments.size(); i++) {
+            Expression argument = callStatement.arguments.get(i);
+            if (argument.dataType != entry.parameterTypes.get(i).type)
+                throw SplError.ArgumentTypeMismatch(callStatement.position, callStatement.procedureName, i + 1, entry.parameterTypes.get(i).type, argument.dataType);
+            if (entry.parameterTypes.get(i).isReference && !(argument instanceof VariableExpression))
+               throw SplError.ArgumentMustBeAVariable(argument.position, callStatement.procedureName, i + 1);
+        }
+    }
+
+    @Override
+    public void visit(UnaryExpression unaryExpression) {
+        unaryExpression.operand.accept(this);
+        if (unaryExpression.operand.dataType == PrimitiveType.boolType)
+            throw SplError.NoSuchOperator(unaryExpression.position, unaryExpression.operator, unaryExpression.operand.dataType);
+        unaryExpression.dataType = unaryExpression.operand.dataType;
+    }
+
+    @Override
+    public void visit(BinaryExpression binaryExpression) {
+        binaryExpression.leftOperand.accept(this);
+        binaryExpression.rightOperand.accept(this);
+        binaryExpression.dataType = binaryExpression.operator.isArithmetic() ? PrimitiveType.intType : PrimitiveType.boolType;
+        if (binaryExpression.leftOperand.dataType != binaryExpression.rightOperand.dataType)
+            throw SplError.NoSuchOperator(binaryExpression.position,
+                    binaryExpression.operator,
+                    binaryExpression.leftOperand.dataType,
+                    binaryExpression.rightOperand.dataType);
+    }
+
+    @Override
+    public void visit(CompoundStatement compoundStatement) {
+        SymbolTable global = table;
+        SymbolTable local = new SymbolTable(global);
+        table = local;
+        compoundStatement.localTable = local;
+        compoundStatement.statements.forEach(x -> x.accept(this));
+        table = global;
+    }
+
+    @Override
+    public void visit(IntLiteral intLiteral) {
+        intLiteral.dataType = PrimitiveType.intType;
+    }
+
+    @Override
+    public void visit(AssignStatement assignStatement) {
+        assignStatement.target.accept(this);
+        assignStatement.value.accept(this);
+
+        if (assignStatement.target.dataType instanceof ArrayType)
+            throw SplError.IllegalAssignmentToArray(assignStatement.position);
+
+        if (assignStatement.target.dataType != assignStatement.value.dataType)
+            throw SplError.IllegalAssignment(assignStatement.position, assignStatement.target.dataType, assignStatement.value.dataType);
+    }
+
+    @Override
+    public void visit(VariableExpression variableExpression) {
+        variableExpression.variable.accept(this);
+        variableExpression.dataType = variableExpression.variable.dataType;
+    }
+
+    @Override
+    public void visit(NamedVariable namedVariable) {
+        Entry entry = table.lookup(namedVariable.name, SplError.UndefinedVariable(namedVariable.position, namedVariable.name));
+        if (!(entry instanceof VariableEntry))
+            throw SplError.NotAVariable(namedVariable.position, namedVariable.name);
+        namedVariable.dataType = ((VariableEntry)entry).type;
+    }
+
+    @Override
+    public void visit(ArrayAccess arrayAccess) {
+        arrayAccess.index.accept(this);
+        if (arrayAccess.index.dataType != PrimitiveType.intType)
+            throw SplError.IndexingWithNonInteger(arrayAccess.position);
+        arrayAccess.array.accept(this);
+        arrayAccess.dataType = ((ArrayType)arrayAccess.array.dataType).baseType;
     }
 
 }
--- tests.sh
+++ tests.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-JAVA=/home/ubuntu/.jdks/openjdk-17.0.1/bin/java;
+JAVA=/home/ubuntu/.jdks/openjdk-17.0.1/bin/java
 REF=./refspl
 
 for t in spl-testfiles/*/*.spl; do
-  printf "\x1B[0mTesting \033[1m$t\x1B[0m.... ";
-  $JAVA -cp spl.jar de.thm.mni.compilerbau.Main $1 $t > output1.txt 2>&1;
-  $REF $1 $t > output2.txt 2>&1;
-  if diff output1.txt output2.txt > /dev/null 2>&1; then
-    printf "\033[32m✓\n";
-  else
+  printf "\x1B[0mTesting \033[3m%s\x1B[0m .... " $t;
+  $JAVA -cp spl.jar de.thm.mni.compilerbau.Main $1 $t > output1.txt 2>output_error1.txt;
+  $REF $1 $t > output2.txt 2>output_error2.txt;
+
+  if ! diff output1.txt output2.txt > log.txt 2>&1; then
     printf "\033[31m✗\n";
+    printf "\x1B[0mSee \033[3mlog.txt \x1B[0mfor details\n"
     break;
   fi
+
+  if ! diff output_error1.txt output_error2.txt > log.txt 2>&1; then
+    printf "\033[31m✗\n";
+    printf "\x1B[0mSee \033[3mlog.txt \x1B[0mfor details\n"
+    break;
+  fi
+
+  # every output is equal
+  printf "\033[32m✓\n";
 done;
-rm output1.txt output2.txt
+rm output*.txt
